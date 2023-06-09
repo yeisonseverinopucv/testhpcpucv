7abda7814d1e3a172cd71b7515031358372f11ae
yeisonseverinopucv <39418692+yeisonseverinopucv@users.noreply.github.com>
2023-06-09 13:22:07 -0400

    Update 001_ejemplo_R.sh

--- 8_Generacion_de_Jobs_avanzados/R/001_ejemplo_R.sh
+++ 8_Generacion_de_Jobs_avanzados/R/001_ejemplo_R.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+#SBATCH -J 001_ejemplo_R
+#SBATCH -p CPU
+#SBATCH -n 4
+#SBATCH -o salida_%j_001_ejemplo_R.out
+#SBATCH -e salida_%j_001_ejemplo_R.err
+#SBATCH --mail-user=yeison.severino@pucv.cl
+#SBATCH --mail-type=ALL
+
+module load R
+srun Rscript rnorm1.R
