95f41c161aadc2f85a42102bd87cc9f594bedc5e
yeisonseverinopucv <39418692+yeisonseverinopucv@users.noreply.github.com>
2023-06-09 12:50:38 -0400

    Update 001_ejemplo_python.sh

--- 8_Generacion_de_Jobs_avanzados/Python/001_ejemplo_python.sh
+++ 8_Generacion_de_Jobs_avanzados/Python/001_ejemplo_python.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+#SBATCH -J 001_ejemplo_python
+#SBATCH -p CPU
+#SBATCH -n 4
+#SBATCH -o salida_%j_001_ejemplo_python.out
+#SBATCH -e salida_%j_001_ejemplo_python.err
+#SBATCH --mail-user=yeison.severino@pucv.cl
+#SBATCH --mail-type=ALL
+
+srun python n_queens_problem_v3_board_4.py
